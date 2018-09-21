d2f8852b7e986712dd517b8edca2f99068483315
agusu <aguss15dt@gmail.com>
2018-09-21 17:10:30 -0300

    más correcciones, agrego lo que hizo fran

--- numerico.py
+++ numerico.py
@@ -1,104 +1,118 @@
-def main():
-    s = [0]*n #semilla
-    m = generarMatriz(n) #matriz 
-    f = generarFuncion(n) #valores de la funcion, termino independiente
-    itersw= dict()
-    tol = 0.01
-    #Se prueban con diferentes w para saber cual es el optimo, el que de menos
-    #iteraciones 
-    for w in range(1,2,0.05): #w del metodo SOR
-        x, nroiter = SOR(m,s,f,w,tol)
-        itersw[w]=nroiter
-    minimo=1
-    for (w, nroiter) in itersw:
-        if nroiter<minimo:
-            minimo=nroiter
-            wopt=w
-    tol = 0.0001
-    x, nroiter = SOR(m,s,f,wopt,tol)
-    
-def generarFuncion(n):
-    f=[0]*n
-    x=[0]*n
-    q=[0]*n
-    g=2 #este es el numero de grupo lo deberiamos cambiar cuando nos digan que grupo somos
-    for i in range(n):
-        x[i]=i/n
-        q[i]=g+(g*g)*(x[i]-x[i]*x[i])
-        f[i]=q[i]/(n*n*n*n) #ya se que es horrible
+import decimal
 
-    f[0]=0
-    f[n-1]=0#bien hardcodeado
-    print (f)
-    return f
-def generarCeros(n):
+
+def main():
+    n = 10
+    semilla = [0] * n  # arbitraria
+    matriz = generar_matriz_inicializada(n)
+    f = generar_termino_independiente(n)
+    # Se busca el w óptimo (menor cantidad de iteraciones)
+    tol = 0.01
+    w = obtener_w_optimo(matriz, semilla, f, tol)
+    tol = 0.0001
+    x, cant_iter = SOR(matriz, semilla, f, w, tol)
+
+
+def drange(x, y, jump):
+    while x < y:
+        yield float(x)
+        x += decimal.Decimal(jump)
+
+
+def obtener_w_optimo(A, x, b, tol):
+    iteraciones_w = dict()
+    w_optimo = 1
+    for w in drange(1, 2, '0.05'):
+        x, cant_iter = SOR(A, x, b, w, tol)
+        iteraciones_w[w] = cant_iter
+    minimo = 1
+    for (w, cant_iter) in iteraciones_w:
+        if cant_iter < minimo:
+            minimo = cant_iter
+            w_optimo = w
+    return w_optimo
+
+
+def generar_termino_independiente(n):
+    b = [0]*n
+    x = [0]*n
+    q = [0]*n
+    g = 2  # este es el numero de grupo lo deberíamos cambiar cuando nos digan que grupo somos
+    for i in range(n):
+        x[i] = i/n
+        q[i] = g+(g**2)*(x[i]-x[i]**2)
+        b[i] = q[i]/(n**n)
+    b[0] = 0
+    b[n-1] = 0  # bien hardcodeado
+    return b
+
+
+def matriz_ceros(n):
     matriz = []
     for i in range(n):
-        vect=[0]*n
-        matriz.append(vect)
+        vector_ceros = [0] * n
+        matriz.append(vector_ceros)
     return matriz
 
-def generarMatriz(n):
+
+def generar_matriz_inicializada(n):
     if n < 5:
         return 0
-    matriz = generarCeros(n)
-    matriz[0][0]=1
-    #1
-    matriz[1][0]=-4
-    matriz[1][1]=5
-    matriz[1][2]=-4
-    matriz[1][3]=1
-    #i
-    k=0
-    for i in range(2,n-2):
-        vect=[1,-4,6,-4,1]
-        for j in range(k,5+k):
-            matriz[i][j]=vect.pop()
-        k+=1
-    #n-1
-    matriz[n-2]=matriz[1][::-1]
-    #n
-    matriz[n-1][n-1]=1
+    matriz = matriz_ceros(n)
+    matriz[0][0] = 1  # i = 0
+    # i = 1:
+    matriz[1][0] = -4
+    matriz[1][1] = 5
+    matriz[1][2] = -4
+    matriz[1][3] = 1
+    # i:
+    k = 0
+    for i in range(2, n - 2):
+        vect = [1, -4, 6, -4, 1]
+        for j in range(k, 5 + k):
+            matriz[i][j] = vect.pop()
+        k += 1
+    matriz[n - 2] = matriz[1][::-1]  # n-1
+    matriz[n - 1][n - 1] = 1  # n
 
 
-
-def SOR(A,x,b,w,tol):
+def SOR(A, x, b, w, tol):
     n = len(A)
     s = x.copy()
-    xant = [None]*n
-    nroiter = 0
-    while(e > tol):
+    cant_iteraciones = 0
+    e = 1
+    while (e > tol):
         xant = x.copy()
         for i in range(n):
-            x[i] = s[i]*(1-w)+w*GS(A[i],s,b[i],i,n)
-        nroiter += 1
+            x[i] = s[i] * (1 - w) + w * gauss_seidel(A[i], s, b[i], i, n)
+        cant_iteraciones += 1
         s = x.copy()
-        e = error(x,xant)
-    return (x, nroiter)
+        e = error(x, xant)
+    return (x, cant_iteraciones)
 
-   
-def GS(a,s,b,i,n):
+
+def gauss_seidel(coeficientes, semilla, b, i, n):
     suma = 0
-    #der
-    j = i +1
-    if (j != n):
-        while (j<n & a[j] != 0):
-            suma += a[j]*s[j]/a[i]
-            j +=1
-    #izq
-    j = i -1
-    if (j != -1):
-        while (j >= 0 & a[j] != 0): 
-            suma += a[j]*s[j]/a[i]
-            j = j-1
-    return b/a[i]-suma
+    # der
+    j = i + 1
+    if j != n:
+        while j < n & coeficientes[j] != 0:
+            suma += coeficientes[j] * semilla[j] / coeficientes[i]
+            j += 1
+    # izq
+    j = i - 1
+    if j != -1:
+        while j >= 0 & coeficientes[j] != 0:
+            suma += coeficientes[j] * semilla[j] / coeficientes[i]
+            j = j - 1
+    return b / coeficientes[i] - suma
 
-def error(x,xant):
+
+def error(x, xant):
     resultado = []
     for i in range(len(x)):
-        resultado.append((x[i]-xant[i]))
-    norma = max(resultado)/max(x)
+        resultado.append((x[i] - xant[i]))
+    norma = max(resultado) / max(x)
     return norma
 
-generarMatriz(10)
-generarFuncion(10)
+generar_matriz_inicializada(10)
